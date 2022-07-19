2123464a4f9674e80fb060232cec89c1db761fe8
Gaurav Narnaware <gauravnarnaware3112003@gmail.com>
2022-07-19 15:55:00 +0530

    add components

--- pages/Component/NavLocal.js
+++ pages/Component/NavLocal.js
@@ -1,24 +1,30 @@
 const Navigation = [
-    {
-        "location":"/",
-        "Name":"Home",
-        "icon":"bi-house-door-fill"
-    },
-    {
-        "location":"/About",
-        "Name":"About",
-        "icon":"bi-info-circle-fill"
-    },
-    {
-        "location":"/Blogs",
-        "Name":"Blog",
-        "icon":"bi-file-earmark-post-fill"
-    },
-    {
-        "location":"/ContactUs",
-        "Name":"Contact Us",
-        "icon":"bi-telephone-fill"
-    }
-]
+  {
+    location: "/",
+    Name: "Home",
+    icon: "bi-house-door-fill",
+  },
+  {
+    location: "/About",
+    Name: "About",
+    icon: "bi-info-circle-fill",
+  },
+  {
+    location: "/Blogs",
+    Name: "Blog",
+    icon: "bi-file-earmark-post-fill",
+  },
+  {
+    location: "/ContactUs",
+    Name: "Contact Us",
+    icon: "bi-telephone-fill",
+  },
+];
+
+import React from "react";
+
+export const NavLocal = () => {
+  return <>hey</>;
+};
 
 export default Navigation;
