a6e184012da8c4024ce7a740723f9cc55722f507
Gaurav Narnaware <gauravnarnaware3112003@gmail.com>
2022-07-19 10:05:58 +0530

    fixes some normal error

--- pages/admin/Feedback.js
+++ pages/admin/Feedback.js
@@ -53,7 +53,7 @@ const AllBlogs = (props) => {
                 {props.Feedback.map((item, index) => {
                   return (
                     <tr className="my-1 border" key={index}>
-                      <th className="w-1as whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 text-left text-center align-middle text-xs font-bold">
+                      <th className="w-1as whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 text-left  align-middle text-xs font-bold">
                         {index + 1}
                       </th>
                       <th className="w-96 whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 text-left align-middle text-xs">
